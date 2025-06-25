c03b49751a71be3858989097adfd1e8bc01f22b0
Alexander Startsev <bizzary.2010@yandex.ru>
2025-06-26 00:23:15 +0300

    Email,Password,Name

---
+++ constants.py
@@ -0,0 +1,11 @@
+MAIN_URL = ('https://stellarburgers.nomoreparties.site')
+
+LOGIN_PAGE = ('https://stellarburgers.nomoreparties.site/login')
+
+PROFILE_CABINET = ('https://stellarburgers.nomoreparties.site/account/profile')
+
+EMAIL_FOR_LOGIN = ('SashaStartsev25000@ya.ru')
+
+PASSWORD_FOR_LOGIN = ('Bizzary333')
+
+NAME_FOR_REGISTRATION = ('Старцев Александр')
